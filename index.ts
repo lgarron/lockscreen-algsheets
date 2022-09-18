370a5580a5bdf88e3031e70d9d196c3830f8401e
Lucas Garron <code@garron.net>
2022-09-17 21:52:28 -0700

    FREEZE.indexed

--- index.ts
+++ index.ts
@@ -15,30 +15,47 @@ function getConfig(configName?: string): TwistyPlayerConfig {
 }
 
 console.log(getConfig("OLL"));
-const table = document.querySelector("alg-sheet table")!;
-let activeRow: HTMLTableRowElement | null = null;
+const table = document.querySelector("alg-sheet .table") as HTMLTableElement;
+// let activeRow: HTMLElement | null = null;
+let activeRow: HTMLTableElement | null = null;
 
 for (const [alg, info] of Object.entries(data.algs)) {
 	const player = new TwistyPlayer({ ...getConfig(info.config), alg });
 	if (activeRow) {
-		const playerTD = activeRow.appendChild(document.createElement("td"));
-		const playerWrapper = playerTD.appendChild(
-			document.createElement("twisty-player-wrapper"),
+		const algTD = activeRow.appendChild(document.createElement("div"));
+		algTD.classList.add("alg", "right");
+		algTD.textContent = alg;
+		const playerTD = activeRow.appendChild(document.createElement("div"));
+		// const playerWrapper = playerTD.appendChild(
+		// 	document.createElement("twisty-player-wrapper"),
+		// );
+		playerTD.appendChild(player);
+		// const hashOLLTD = activeRow.appendChild(document.createElement("div"));
+		const hashOLLTextWrapper = activeRow.appendChild(
+			document.createElement("div"),
 		);
-		playerWrapper.appendChild(player);
-		playerTD.rowSpan = 2;
-		const tr = table.appendChild(document.createElement("tr"));
-		tr.appendChild(document.createElement("td")).textContent = alg;
+		hashOLLTextWrapper.classList.add("hash-oll");
+		hashOLLTextWrapper.textContent = "#URF";
 		activeRow = null;
 	} else {
-		activeRow = table.appendChild(document.createElement("tr"));
-		const playerTD = activeRow.appendChild(document.createElement("td"));
-		const playerWrapper = playerTD.appendChild(
-			document.createElement("twisty-player-wrapper"),
+		const activeRoww = table; // table.appendChild(document.createElement("tr"));
+		activeRow = activeRoww;
+		// const hashOLLTD = activeRow.appendChild(document.createElement("div"));
+		const hashOLLTextWrapper = activeRoww.appendChild(
+			document.createElement("div"),
 		);
-		playerWrapper.appendChild(player);
-		playerTD.rowSpan = 2;
-		activeRow.appendChild(document.createElement("td")).textContent = alg;
+		hashOLLTextWrapper.classList.add("hash-oll");
+		hashOLLTextWrapper.classList.add("cw");
+		hashOLLTextWrapper.textContent = "#URF";
+
+		const playerTD = activeRoww.appendChild(document.createElement("div"));
+		// const playerWrapper = playerTD.appendChild(
+		// 	document.createElement("twisty-player-wrapper"),
+		// );
+		playerTD.appendChild(player);
+		const algTD = activeRoww.appendChild(document.createElement("div"));
+		algTD.classList.add("alg");
+		algTD.textContent = alg;
 	}
 	// const algCase = algSheet.appendChild(document.createElement("alg-case"));
 	// const twistyPlayerWrapper = algCase.appendChild(

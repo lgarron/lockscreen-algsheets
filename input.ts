d7481624734ff02733afce260fc072ed94f7ea69
Lucas Garron <code@garron.net>
2022-09-17 23:55:38 -0700

    FREEZE.unindexed

--- input.ts
+++ input.ts
@@ -3,30 +3,63 @@ import { KeyboardPuzzle } from "cubing/bluetooth";
 import { KState } from "cubing/kpuzzle";
 import { cube3x3x3 } from "cubing/puzzles";
 import "cubing/twisty";
-import { TwistyPlayer } from "cubing/twisty";
+import { TwistyAlgViewer, TwistyPlayer } from "cubing/twisty";
 
 const player = document.body.appendChild(
 	new TwistyPlayer({
 		controlPanel: "none",
+		experimentalStickering: "OLL",
+	}),
+);
+player.addEventListener("keydown", (e) => {
+	switch (e.code) {
+		case "Enter": {
+			recordOLL();
+			break;
+		}
+		case "Space": {
+			player.alg = "";
+			inversePlayer.alg = "";
+		}
+	}
+});
+
+const inversePlayer = document.body.appendChild(
+	new TwistyPlayer({
+		controlPanel: "none",
+		experimentalStickering: "OLL",
 	}),
 );
 player.tabIndex = 0;
 player.focus();
 const kbp = new KeyboardPuzzle(player);
 kbp.addAlgLeafListener((e) => {
-	player.experimentalAddAlgLeaf(e.latestAlgLeaf, { coalesce: true });
+	player.experimentalAddAlgLeaf(e.latestAlgLeaf, {
+		coalesce: true,
+		puzzleSpecificAlgSimplificationInfo:
+			cube3x3x3.puzzleSpecificAlgSimplificationInfo,
+	});
+	inversePlayer.experimentalModel.alg.set(
+		(async () => (await player.experimentalModel.alg.get()).alg.invert())(),
+	);
+	inversePlayer.timestamp = "end";
 });
+document.body.appendChild(document.createElement("br"));
+document.body.appendChild(document.createElement("br"));
+document.body.appendChild(new TwistyAlgViewer({ twistyPlayer: player }));
+document.body.appendChild(document.createElement("br"));
+document.body.appendChild(document.createElement("br"));
 
 const algHelpers = {
 	CORNERS: {
+		O: "",
+		H: "U R U2' R' U' R U R' U' R U' R' U'",
 		S: "R U2' R' U' R U' R'",
 		A: "R U R' U R U2 R'",
-		H: "U R U2' R' U' R U R' U' R U' R' U'",
 		L: "U' R U2' R' U' R U R' U' R U R' U' R U' R' U",
 		P: "B U2 B2 U' B2 U' B2 U2' B",
-		T: "U2 L' U2' L U L' U L R U2 R' U' R U' R' U' U2",
 		U: "U U R U R' U R U2' R' L' U' L U' L' U2 L U' U' U'",
-		O: "",
+		T: "U2 L' U2' L U L' U L R U2 R' U' R U' R' U' U2",
 	},
 	EDGES: {
 		O: "",
@@ -50,19 +83,23 @@ function toOriHash(t: KState) {
 
 const OLLs: Record<string, { name: string; AUF: string }> = {};
 const kpuzzle = await cube3x3x3.kpuzzle();
-for (const AUF of [".", "U", "U2", "U'"]) {
-	const aufTransformation = kpuzzle.algToTransformation(
-		Alg.fromString(AUF),
-	).invert();
-	for (const [eoName, eoAlg] of Object.entries(algHelpers.EDGES)) {
-		const edgeTransformation = aufTransformation.applyAlg(
-			Alg.fromString(eoAlg).invert(),
+for (const [eoName, eoAlg] of Object.entries(algHelpers.EDGES)) {
+	const edgeTransformation = kpuzzle.algToTransformation(
+		Alg.fromString(eoAlg).invert(),
+	);
+	for (const [coName, coAlg] of Object.entries(algHelpers.CORNERS)) {
+		const ollTransformation = edgeTransformation.applyAlg(
+			Alg.fromString(coAlg).invert(),
 		);
-		for (const [coName, coAlg] of Object.entries(algHelpers.CORNERS)) {
-			const ollTransformation = edgeTransformation.applyAlg(
-				Alg.fromString(coAlg).invert(),
+		for (const AUF of [".", "U", "U2", "U'"]) {
+			const aufTransformation = kpuzzle.algToTransformation(
+				Alg.fromString(AUF),
+			).invert();
+			const oriHash = toOriHash(
+				aufTransformation.applyTransformation(
+					ollTransformation,
+				).invert().toKState(),
 			);
-			const oriHash = toOriHash(ollTransformation.invert().toKState());
 			const hashOLLName = `${coName}${eoName}`;
 			// console.log(`${AUF} ${hashOLLName}`, oriHash);
 			OLLs[oriHash] ??= { name: hashOLLName, AUF };
@@ -72,9 +109,9 @@ for (const AUF of [".", "U", "U2", "U'"]) {
 
 // console.log(Object.keys(OLLs).length);
 
-const button = document.body.appendChild(document.createElement("button"));
-button.textContent = "Compute!";
-button.addEventListener("click", recordOLL);
+// const button = document.body.appendChild(document.createElement("button"));
+// button.textContent = "Compute!";
+// button.addEventListener("click", recordOLL);
 
 function getStored(hashName: string): string | null {
 	return localStorage[hashName] ?? null;
@@ -82,7 +119,9 @@ function getStored(hashName: string): string | null {
 
 function setStored(hashName: string, alg: string): void {
 	localStorage[hashName] = alg;
-	document.getElementById(hashName)!.textContent = alg.toString();
+	document.getElementById(
+		hashName,
+	)!.textContent = `${alg.toString()} #${hashName}`;
 }
 
 const table = document.body.appendChild(document.createElement("table"));
@@ -92,12 +131,12 @@ for (const co of Object.keys(algHelpers.CORNERS)) {
 		const td = tr.appendChild(document.createElement("td"));
 		const hashName = co + eo;
 		td.id = hashName;
-		td.textContent = getStored(hashName) ?? ".";
+		td.textContent = `${getStored(hashName) ?? ""} #${hashName}`;
 	}
 }
 
 async function recordOLL() {
-	const [state, alg] = await Promise.all([
+	let [state, { alg }] = await Promise.all([
 		player.experimentalModel.currentState.get(),
 		player.experimentalModel.alg.get(),
 	]);
@@ -110,6 +149,6 @@ async function recordOLL() {
 	// localStorage[lookup.name] ??= `${lookup.AUF} ${alg}`;
 	setStored(
 		lookup.name,
-		`${lookup.AUF !== "." ? `(${lookup.AUF}) ` : ""}${alg.alg}`,
+		`${lookup.AUF !== "." ? `(${lookup.AUF}) ` : ""}${alg}`,
 	);
 }

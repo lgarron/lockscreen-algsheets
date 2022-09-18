3517937ef94a5bb5aa383785f1295817d644b116
Lucas Garron <code@garron.net>
2022-09-17 22:57:49 -0700

    FREEZE.unindexed

--- input.ts
+++ input.ts
@@ -14,7 +14,7 @@ player.tabIndex = 0;
 player.focus();
 const kbp = new KeyboardPuzzle(player);
 kbp.addAlgLeafListener((e) => {
-	player.experimentalAddAlgLeaf(e.latestAlgLeaf);
+	player.experimentalAddAlgLeaf(e.latestAlgLeaf, { coalesce: true });
 });
 
 const algHelpers = {
@@ -63,22 +63,53 @@ for (const AUF of [".", "U", "U2", "U'"]) {
 				Alg.fromString(coAlg).invert(),
 			);
 			const oriHash = toOriHash(ollTransformation.invert().toKState());
-			const hashOLLName = `#${coName}${eoName}`;
-			console.log(`${AUF} #${hashOLLName}`, oriHash);
+			const hashOLLName = `${coName}${eoName}`;
+			// console.log(`${AUF} ${hashOLLName}`, oriHash);
 			OLLs[oriHash] ??= { name: hashOLLName, AUF };
 		}
 	}
 }
 
-console.log(Object.keys(OLLs).length);
-
-async function computeOLL() {
-	const state = await player.experimentalModel.currentState.get();
-	const oriHash = toOriHash(state);
-	console.log();
-	console.log(OLLs[oriHash]);
-}
+// console.log(Object.keys(OLLs).length);
 
 const button = document.body.appendChild(document.createElement("button"));
 button.textContent = "Compute!";
-button.addEventListener("click", computeOLL);
+button.addEventListener("click", recordOLL);
+
+function getStored(hashName: string): string | null {
+	return localStorage[hashName] ?? null;
+}
+
+function setStored(hashName: string, alg: string): void {
+	localStorage[hashName] = alg;
+	document.getElementById(hashName)!.textContent = alg.toString();
+}
+
+const table = document.body.appendChild(document.createElement("table"));
+for (const co of Object.keys(algHelpers.CORNERS)) {
+	const tr = table.appendChild(document.createElement("tr"));
+	for (const eo of Object.keys(algHelpers.EDGES)) {
+		const td = tr.appendChild(document.createElement("td"));
+		const hashName = co + eo;
+		td.id = hashName;
+		td.textContent = getStored(hashName) ?? ".";
+	}
+}
+
+async function recordOLL() {
+	const [state, alg] = await Promise.all([
+		player.experimentalModel.currentState.get(),
+		player.experimentalModel.alg.get(),
+	]);
+	const oriHash = toOriHash(
+		state.experimentalToTransformation()!.invert().toKState(),
+	);
+	// console.log();
+	const lookup = OLLs[oriHash];
+	console.log(oriHash, lookup, alg);
+	// localStorage[lookup.name] ??= `${lookup.AUF} ${alg}`;
+	setStored(
+		lookup.name,
+		`${lookup.AUF !== "." ? `(${lookup.AUF}) ` : ""}${alg.alg}`,
+	);
+}
